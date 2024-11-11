f2445ce69782e65810ccee99af18b3cad22437eb
Noah Westerberg <noah.westerberg2@elev.ga.ntig.se>
2024-11-11 14:23:13 +0100

    veiw todo items

--- app.rb
+++ app.rb
@@ -9,6 +9,52 @@ class App < Sinatra::Base
     end
     
     get '/' do
+        @todo = db.execute("SELECT * FROM todoList")
+
+        # time1 - time2
+        # returns: string; either: seconds, minuts, houres, days, weeks, months, years between the two times
+        def time_difference(time1, time2)
+            difference = time1 - time2
+            difference_seconds = (difference.to_f * 86400).abs
+            
+            p time1
+            p time2
+            p difference_seconds
+            end_s = ""
+
+            if difference_seconds < 60
+                seconds = (difference * 86400).to_i
+                if seconds > 1
+                end_s = "s"
+                end
+                return "#{seconds} second" + end_s
+            elsif difference_seconds < 3600
+                minuts = (difference * 1440).to_i
+                if minuts > 1
+                    end_s = "s"
+                end
+                return "#{minuts} minut" + end_s
+            elsif difference_seconds < 86400
+                hours = (difference * 24).to_i
+                if hours > 1
+                    end_s = "s"
+                end
+                return "#{hours} hour" + end_s
+            elsif difference.to_i < 365
+                days = difference.to_i
+                if days > 1
+                    end_s = "s"
+                end
+                return "#{days} day" + end_s
+            else
+                years = (difference / 365).to_i
+                if years > 1
+                    end_s = "s"
+                end
+                return "#{years} year" + end_s
+            end
+        end
+
         erb(:"pages/index")
     end
     
