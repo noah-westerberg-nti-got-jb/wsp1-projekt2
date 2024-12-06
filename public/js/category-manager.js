4f7fa79fe65db0c016a04f20e0bb7284381ec90b
Noah Westerberg <noah.westerberg2@elev.ga.ntig.se>
2024-12-06 14:10:43 +0100

    move category-manager functions to separate javascript file

---
+++ public/js/category-manager.js
@@ -0,0 +1,76 @@
+collapse = (button, id) => {
+    if (button.collapsed) {
+        document.getElementById("subcategories-" + id).style.display = "block";
+        button.innerHTML = "collapse";
+    } 
+    else {
+        document.getElementById("subcategories-" + id).style.display = "none";
+        button.innerHTML = "expand";
+    }
+    button.collapsed = !button.collapsed
+};
+
+deleteCategory = (id) => {
+
+};
+
+let child_to_append = null;
+changeParent = (id) => {
+    document.querySelectorAll('button').forEach((button) => {
+        if (button.className == "set-parent-button") {
+            button.style.display = "";
+        }
+        else {
+            button.style.display = "none";
+        }
+    });
+
+    child_to_append = document.getElementById(id);
+};
+
+setParent = (id) => {
+    const append_id = (id == null) ? "categorylist-innercontainer" : "subcategories-" + id;
+    document.getElementById(append_id).appendChild(child_to_append);
+    
+    document.querySelectorAll('button').forEach((button) => {
+        if (button.className == "set-parent-button") {
+            button.style.display = "none";
+        }
+        else {
+            button.style.display = "";
+        }
+    });
+};
+
+let new_id_num = 0;
+createCategory = (parent_id) => {
+    new_category = document.createElement('div');
+    new_category.className= "categorylist-item";
+    const id = "new" + new_id_num++;
+    new_category.id = id;
+
+    new_category.innerHTML = `
+        <div class="category-options">
+            <div class="categorylist-item-left">
+                <button type="button" onclick="collapse(this, '${id}')" collapsed="false">collapse</button>
+                <span class="category-list-item-name"><input type="text" required></span>
+                <button type="button" class="set-parent-button" onclick="setParent('${id}')" style="display: none;">append</button>
+                <button type="button" onclick="changeParent('${id}')">change parent</button>
+                <button type="button" onclick="createCategory('${id}')">new child</button>
+                <button type="button" id="delete-button" onclick="deleteCategory('${id}')">delete</button>
+            </div>
+            <div class="categorylist-item-right">
+                <span class="text-color-area">
+                    Text-color: <input type="color">
+                </span>
+                <span class="background-color-area">
+                    Background-color: <input type="color">
+                </span>
+            </div>
+        </div>
+        <div class="sub-categories" id="subcategories-${id}"></div>
+    `;
+
+    parent_element_id = (parent_id == null) ? "categorylist-innercontainer" : "subcategories-" + parent_id;
+    document.getElementById(parent_element_id).appendChild(new_category);
+};
